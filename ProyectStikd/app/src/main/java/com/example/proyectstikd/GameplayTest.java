63a282abbcbabe6a5d4983b11f6593d72fec4ccd
Srmaquieira14 <39341726+Srmaquieira14@users.noreply.github.com>
2020-01-21 13:49:02 +0100

    Inicio
    Trabajo del día 1

---
+++ ProyectStikd/app/src/main/java/com/example/proyectstikd/GameplayTest.java
@@ -0,0 +1,27 @@
+package com.example.proyectstikd;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameplayTest extends SurfaceView implements SurfaceHolder.Callback{
+
+    public GameplayTest(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
---
+++ ProyectStikd/app/src/main/java/com/example/proyectstikd/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.proyectstikd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView=getWindow().getDecorView();
+        int opciones= View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(opciones);
+
+        GameplayTest screen = new GameplayTest(this);
+        screen.setKeepScreenOn(true);
+        setContentView(screen);
+    }
+}
