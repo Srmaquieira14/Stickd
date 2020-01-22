796b851def0a321ac07314e8c0d49660873f62d0
Srmaquieira14 <39341726+Srmaquieira14@users.noreply.github.com>
2020-01-22 10:32:11 +0100

    Inicio 2
    Siguiendo con el repaso inicial

--- ProyectStikd/app/src/main/java/com/example/proyectstikd/GameplayTest.java
+++ ProyectStikd/app/src/main/java/com/example/proyectstikd/GameplayTest.java
@@ -1,23 +1,57 @@
 package com.example.proyectstikd;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 public class GameplayTest extends SurfaceView implements SurfaceHolder.Callback{
+    private SurfaceHolder surfaceHolder;
+    private Context context;
+    private firstThread physics;
+    private int screenWidth = 1;
+    private int screenHeight = 1;
+
+
 
     public GameplayTest(Context context) {
         super(context);
+
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+        this.context = context;
+        physics = new firstThread();
+        setFocusable(true);
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getRealMetrics(dm);
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+    }
+
+    public void updatePhysics(){
+
+    }
+
+    public void drawIt(Canvas c) {
+        try{
+            c.drawRect(new Rect(),null);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
+    public void surfaceCreated(SurfaceHolder holder){
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+        //screenWidth = width;
+        //screenHeight = height;
     }
 
     @Override
---
+++ ProyectStikd/app/src/main/java/com/example/proyectstikd/firstThread.java
@@ -0,0 +1,21 @@
+package com.example.proyectstikd;
+
+public class firstThread extends Thread{
+    private boolean funcionando = false;
+
+    public boolean isFuncionando() {
+        return funcionando;
+    }
+
+    public void setFuncionando(boolean funcionando) {
+        this.funcionando = funcionando;
+    }
+
+    public firstThread(){
+    }
+
+    public void run(){
+
+    }
+
+}
